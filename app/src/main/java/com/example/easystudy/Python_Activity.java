3c997a4914b0cf49acef995183e0a9be81b2eb76
Justrygh <justgithub@gmail.com>
2020-01-15 15:50:43 +0200

    Merge branch 'FinalBranch' of C:\Users\Computer\Desktop\Application-master with conflicts.

---
+++ app/src/main/java/com/example/easystudy/Python_Activity.java
@@ -0,0 +1,14 @@
+package com.example.easystudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Python_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_python_);
+    }
+}
